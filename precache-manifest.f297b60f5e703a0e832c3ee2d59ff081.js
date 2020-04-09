0d5903fd22278ce106687beee6ce6743e2276524
Jeremy Curtis <jeremy.curtis808@gmail.com>
2020-04-09 16:16:57 -0500

    Updates

---
+++ precache-manifest.f297b60f5e703a0e832c3ee2d59ff081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24bcc2ae63238d9b264752d8b0aac08",
+    "url": "/jcurtis808/pokemon-api/index.html"
+  },
+  {
+    "revision": "6e921c6f8826602b8a1d",
+    "url": "/jcurtis808/pokemon-api/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "40a9b7c87c53b45d4d8a",
+    "url": "/jcurtis808/pokemon-api/static/css/main.b39b869d.chunk.css"
+  },
+  {
+    "revision": "6e921c6f8826602b8a1d",
+    "url": "/jcurtis808/pokemon-api/static/js/2.e6c97861.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jcurtis808/pokemon-api/static/js/2.e6c97861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a9b7c87c53b45d4d8a",
+    "url": "/jcurtis808/pokemon-api/static/js/main.f2e09152.chunk.js"
+  },
+  {
+    "revision": "913e0f14e4ceb2e1eb4c",
+    "url": "/jcurtis808/pokemon-api/static/js/runtime-main.94a24146.js"
+  }
+]);
